8f10c00d55f2d7305e2698a001e8afc39bcebc13
Ochoagram <ricardogochoa@me.com>
2023-08-14 23:06:23 -0600

    test

--- Python/000_imc.py
+++ Python/000_imc.py
@@ -5,7 +5,7 @@ if peso <= 0 or estatura <= 0:
     print("Los valores deben de ser mayor a 0")
 else:
     estatura_final = estatura
-    estatura_al_cuadrado = estatura_final* estatura_final
+    estatura_al_cuadrado = estatura_final*estatura_final
     imc = peso/estatura_al_cuadrado
 
 print(f"Tu ídice de masa corporal es:, {imc: 0.3}")
