97770b9ea2749f7c433f8fcf6fbb6dd8c60d952e
Ochoagram <ricardogochoa@me.com>
2023-11-06 13:04:35 -0600

    ejercicios

--- c++/002.cpp
+++ c++/002.cpp
@@ -2,9 +2,11 @@
 // Este programa es Tarea datos estructurados
 #include <stdio.h>
 #include <iostream>
+#include <cstdlib> // Librería numeros random
 using namespace std;
 
 int edades() {
+    //Declarar Areglo
     int arreglo_edades[10]; 
 
     // Asignar valores al arreglo
@@ -19,6 +21,7 @@ int edades() {
 }
 
 int suma_edades() {
+    //Declarar Variables
     int arreglo_edades[10];
     int edad = 0;
     int suma_edades = 0;
@@ -33,8 +36,44 @@ int suma_edades() {
     }
 
     // Mostrar los valores
-        cout << "Suma de edades: " << suma_edades << std::endl;
+    cout << "Suma de edades: " << suma_edades << std::endl;
 }
+
+int promedio_random(){
+    int numeros_random[50];
+    int promedio = 0;
+
+    for (int i = 0; i < 50; ++i) {
+        numeros_random[i] = std::rand() % 201;
+        promedio = promedio += numeros_random[i];
+    }
+
+    // Mostrar los valores
+    cout << "El promedio de los valores aleatorios es: " << promedio / 50;
+}
+
+int valor_mayor(){
+    int numeros_random[25];
+    int numero_mayor = 0;
+    int index_numero = 0;
+
+    for (int i = 0; i < 25; ++i) {
+        numeros_random[i] = std::rand() % 501;
+         
+        if(numero_mayor <= numeros_random[i]){
+            numero_mayor = numeros_random[i];
+            index_numero = i;
+        }
+    }
+
+    // Mostrar los valores
+    cout << "El valor más grande el arreglo es : " << numero_mayor << "y está en la posición:" << index_numero << "\n";
+    
+    for (int i = 0; i < 25; i++) {
+        cout << i <<":" << numeros_random[i] << std::endl;
+    }
+}
+
 
 int main() {
     // Declarar variables
@@ -44,15 +83,21 @@ int main() {
     // Procesos
 
     // Salidas
-    cout << "Edades:";
-    edades();
+    // cout << "Edades:";
+    // edades();
+    // cout << "\n";
+
+    // cout << "Suma de edades:";
+    // suma_edades();
+    // cout << "\n";
+
+    // cout << "Promedio de random:";
+    // promedio_random();
+    // cout << "\n";
+
+    cout << "Valor mayor:";
+    valor_mayor();
     cout << "\n";
-
-    cout << "Suma de edades:";
-    suma_edades();
-    cout << "\n";
-
-
 
     return 0;
 }
